43e70f468a277ee09f04ef4fc88f6bdb1a044f6a
Misheel <misheelcoding@gmail.com>
2025-09-17 15:19:32 +0300

    feat: AccounAdd
    created component AccountAdd.vue
    
    :)

--- src/types/accountItem.ts
+++ src/types/accountItem.ts
@@ -2,6 +2,11 @@ export const RECORD_TYPES = ['LDAP', 'local'] as const
 
 export type RecordTypes = (typeof RECORD_TYPES)[number]
 
+export const RECORD_TYPE_TEXT = {
+  LDAP: 'LDAP',
+  local: 'Локальная',
+} as const satisfies Record<RecordTypes, string>
+
 export interface Account {
   id: string
   label: { text: string }[] | null
